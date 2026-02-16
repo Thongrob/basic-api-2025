4e08d0c4f00479d9cfcf2346fc066a81e7851697
Thongrob Tipsurat <thongrob.tsr@gmail.com>
2026-02-16 15:52:57 +0700

    Edit payload

---
+++ controllers/resData.js
@@ -0,0 +1,30 @@
+
+
+//สร้าง controller
+
+exports.resdata = (req, res, next) => {
+    res.json([
+      {
+       "id_emp":1,
+        "firstname":"Joe",
+        "lastname":"Limp",
+        "department":"IT"
+       },
+       {
+       "id_emp":2,
+        "firstname":"Marta",
+        "lastname":"Spread Brow",
+        "department":"Engineering"
+       },
+       {
+       "id_emp":3,
+        "firstname":"Scout",
+        "lastname":"Lang",
+        "department":"HR"
+       }
+
+    ])
+}
+
+
+
